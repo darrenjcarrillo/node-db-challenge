0bddf069bcbd126486e50165f0441c48fc1d3ae8
Darren J Carrillo <darrenjcarrillo@gmail.com>
2019-11-08 09:03:46 -0800

    Migrations Created

---
+++ data/migrations/20191108083347_projectsTable.js
@@ -0,0 +1,66 @@
+exports.up = function(knex) {
+  return (
+    knex.schema
+
+      // Projects Table
+      .createTable("projects", tbl => {
+        tbl.increments(); // Primary key
+        tbl.string("projectName", 255).notNullable();
+        tbl.boolean("completed");
+        // tbl
+        //   .integer("resources_id")
+        //   .unsigned()
+        //   .references("id")
+        //   .inTable("resources")
+        //   .onDelete("RESTRICT")
+        //   .onUpdate("CASCADE");
+      })
+
+      // Resources Table
+      .createTable("resources", tbl => {
+        tbl.increments(); // Primary key
+        tbl.string("resourcesName", 255).notNullable();
+        // tbl
+        //   .integer("project_id")
+        //   .unsigned()
+        //   .references("id")
+        //   .inTable("projects")
+        //   .onDelete("RESTRICT")
+        //   .onUpdate("CASCADE");
+      })
+
+      .createTable("task", tbl => {
+        tbl.increments(); // Primary key
+        tbl.string("taskName", 255).notNullable();
+        tbl.string("Description", 255).notNullable();
+      })
+
+      // Project Task Table
+      .createTable("project_task", tbl => {
+        tbl.increments(); // Primary key
+        tbl
+          .integer("project_id")
+          .unsigned()
+          .references("id")
+          .inTable("projects")
+          .onDelete("RESTRICT")
+          .onUpdate("CASCADE");
+        tbl
+          .integer("resources_id")
+          .unsigned()
+          .references("id")
+          .inTable("resources")
+          .onDelete("RESTRICT")
+          .onUpdate("CASCADE");
+        tbl
+          .integer("task_id")
+          .unsigned()
+          .references("id")
+          .inTable("task")
+          .onDelete("RESTRICT")
+          .onUpdate("CASCADE");
+      })
+  );
+};
+
+exports.down = function(knex) {};
